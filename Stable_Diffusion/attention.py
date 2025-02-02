7ed5c2fd96c4c5793932e5cffd8ae9a31bd83601
Ebad Sayed <95970293+ES7@users.noreply.github.com>
2025-02-02 11:58:47 +0530

    Update attention.py

--- Stable_Diffusion/attention.py
+++ Stable_Diffusion/attention.py
@@ -18,8 +18,6 @@ class SelfAttention(nn.Module):
 
         # (Batch_Size, Seq_Len, Dim)
         input_shape = x.shape 
-        
-        # (Batch_Size, Seq_Len, Dim)
         batch_size, sequence_length, d_embed = input_shape 
 
         # (Batch_Size, Seq_Len, H, Dim / H)
@@ -45,8 +43,6 @@ class SelfAttention(nn.Module):
         # Divide by d_k (Dim / H). 
         # (Batch_Size, H, Seq_Len, Seq_Len) -> (Batch_Size, H, Seq_Len, Seq_Len)
         weight /= math.sqrt(self.d_head) 
-
-        # (Batch_Size, H, Seq_Len, Seq_Len) -> (Batch_Size, H, Seq_Len, Seq_Len)
         weight = F.softmax(weight, dim=-1) 
 
         # (Batch_Size, H, Seq_Len, Seq_Len) @ (Batch_Size, H, Seq_Len, Dim / H) -> (Batch_Size, H, Seq_Len, Dim / H)
@@ -87,14 +83,12 @@ class CrossAttention(nn.Module):
         q = self.q_proj(x)
         # (Batch_Size, Seq_Len_KV, Dim_KV) -> (Batch_Size, Seq_Len_KV, Dim_Q)
         k = self.k_proj(y)
-        # (Batch_Size, Seq_Len_KV, Dim_KV) -> (Batch_Size, Seq_Len_KV, Dim_Q)
         v = self.v_proj(y)
 
         # (Batch_Size, Seq_Len_Q, Dim_Q) -> (Batch_Size, Seq_Len_Q, H, Dim_Q / H) -> (Batch_Size, H, Seq_Len_Q, Dim_Q / H)
         q = q.view(interim_shape).transpose(1, 2) 
         # (Batch_Size, Seq_Len_KV, Dim_Q) -> (Batch_Size, Seq_Len_KV, H, Dim_Q / H) -> (Batch_Size, H, Seq_Len_KV, Dim_Q / H)
         k = k.view(interim_shape).transpose(1, 2) 
-        # (Batch_Size, Seq_Len_KV, Dim_Q) -> (Batch_Size, Seq_Len_KV, H, Dim_Q / H) -> (Batch_Size, H, Seq_Len_KV, Dim_Q / H)
         v = v.view(interim_shape).transpose(1, 2) 
         
         # (Batch_Size, H, Seq_Len_Q, Dim_Q / H) @ (Batch_Size, H, Dim_Q / H, Seq_Len_KV) -> (Batch_Size, H, Seq_Len_Q, Seq_Len_KV)
@@ -102,8 +96,6 @@ class CrossAttention(nn.Module):
         
         # (Batch_Size, H, Seq_Len_Q, Seq_Len_KV)
         weight /= math.sqrt(self.d_head)
-        
-        # (Batch_Size, H, Seq_Len_Q, Seq_Len_KV)
         weight = F.softmax(weight, dim=-1)
         
         # (Batch_Size, H, Seq_Len_Q, Seq_Len_KV) @ (Batch_Size, H, Seq_Len_KV, Dim_Q / H) -> (Batch_Size, H, Seq_Len_Q, Dim_Q / H)
